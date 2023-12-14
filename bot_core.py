5ccc7d2c1c7be39f5b3f2c769dd85f026dbe0923
aurrog <gnusovme@gmail.com>
2023-12-14 16:47:47 +0300

    update

--- bot_core.py
+++ bot_core.py
@@ -10,9 +10,13 @@ bot = telebot.TeleBot(st.BOT_TOKEN)
 def get_text_messages(message):
     if message.text not in st.BOT_COMMAND_LIST:
         object_url = bf.find_url(message.text)
-        response = bf.parce_information_for_bot(object_url)
-        bf.get_url_to_img(object_url)
-        bot.send_photo(message.from_user.id, open('images/img.jpg', 'rb'), caption=response)
+        if object_url:
+            response = bf.parce_information_for_bot(object_url)
+            bf.get_url_to_img(object_url)
+            markup = telebot.types.InlineKeyboardMarkup()
+            button1 = telebot.types.InlineKeyboardButton(message.text, url=object_url)
+            markup.add(button1)
+            bot.send_photo(message.from_user.id, open('images/img.jpg', 'rb'), caption=response, reply_markup=markup)
     if message.text == '/start':
         bot.send_message(message.from_user.id, 'Введите название лекарства, а бот выдаст его цену и наличие в аптеках.')
     bf.load_logs(message)
