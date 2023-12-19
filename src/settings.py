5352358b064aa5f3dba9b9a8694f5fc31f53bf6b
aurrog <gnusovme@gmail.com>
2023-12-19 21:28:57 +0300

    update

--- src/settings.py
+++ src/settings.py
@@ -6,11 +6,6 @@ BOT_COMMAND_LIST = ['/start', '/help', '/set_city', '/my_city']
 GOOGLE_TEMPLATE = 'https://www.google.com/search?q='
 OZERKI_TEMPLATE = '/url?q=https://ozerki.ru/sankt-peterburg/catalog/product/'
 
-product_h1_name_class = 'sc-af7b87e6-1 geivEL'
-product_div_price_class = 'product-price__base-price'
-product_div_addresses_class = 'sc-605479e2-4 gqLEMG'
-product_div_image_class = 'sc-e1f8e4dd-5 kUQYWt'
-
 CITY_LIST = ['москва', 'санкт-петербург', 'новосибирск', 'саратов', 'краснодар', 'нижний ковгород', 'волгоград', 'орел',
              'белгород', 'владимир', 'вологда', 'воронеж', 'калуга', 'петропавловск-камчатский', 'кемерово',
              'красноярск', 'курск', 'омск', 'пенза', 'псков', 'майкоп', 'уфа', 'ростов-на-дону', 'самара', 'саранск',
